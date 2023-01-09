13ff2dbed4ff74c9eae38319116ed438b660e8cd
Aryton Tediarjo <35942857+arytontediarjo@users.noreply.github.com>
2023-01-08 19:32:02 -0800

    update pkgdown

--- R/aws_s3_create_bucket.R
+++ R/aws_s3_create_bucket.R
@@ -1,10 +1,14 @@
 #' Function to create S3 bucket
 #' @description create S3 bucket via API
 #' @param bucket s3 bucket
+#' @param namespace_bucket namespace bucket for prod/dev
 #' @param ... additional parameter passed to s3 create_bucket endpoint
 #' @export
-aws_s3_create_bucket <- function(bucket, ...){
+aws_s3_create_bucket <- function(bucket, namespace_bucket, ...){
   tryCatch({
+    if(namespace_bucket){
+      bucket <- aws_namespace(bucket)
+    }
     s3obj <- paws::s3()
     s3obj$create_bucket(Bucket = bucket, ...)
     profile_name <- Sys.getenv('AWS_PROFILE')
